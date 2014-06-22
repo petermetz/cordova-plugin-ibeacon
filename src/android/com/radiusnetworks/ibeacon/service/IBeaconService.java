2c4b820f092390e0830c37746d125e30370e9a42
Tree <mrtree@gmail.com>
2014-06-22 09:44:36 +0100

    Functional Android support for monitoring and ranging

--- src/android/com/radiusnetworks/ibeacon/service/IBeaconService.java
+++ src/android/com/radiusnetworks/ibeacon/service/IBeaconService.java
@@ -40,7 +40,6 @@ import android.os.Messenger;
 import android.util.Log;
 
 import com.radiusnetworks.bluetooth.BluetoothCrashResolver;
-import com.radiusnetworks.ibeacon.BuildConfig;
 import com.radiusnetworks.ibeacon.IBeacon;
 import com.radiusnetworks.ibeacon.IBeaconManager;
 import com.radiusnetworks.ibeacon.Region;
@@ -198,7 +197,7 @@ public class IBeaconService extends Service {
 
     @Override
     public void onCreate() {
-        Log.i(TAG, "iBeaconService version "+ BuildConfig.VERSION_NAME+" is starting up");
+        Log.i(TAG, "iBeaconService is starting up");
         getBluetoothAdapter();
         bluetoothCrashResolver = new BluetoothCrashResolver(this);
         bluetoothCrashResolver.start();
